b75baa2ab3fd45cf6b14bf1dd6f06574c296ab85
sparisca05 <simon.parisca@eia.edu.co>
2024-09-30 23:24:25 -0500

    otro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import TopNavigation from './components/TopNavigation'
 import SummarySection from './components/SummarySection'
 import ContactSection from './components/ContactSection'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
